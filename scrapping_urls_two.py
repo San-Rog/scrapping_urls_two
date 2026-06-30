8d12ce1bfc991df1d84a2b8bcab97562fce66d92
San-Rog <118987989+San-Rog@users.noreply.github.com>
2026-06-30 13:45:56 -0300

    Update scrapping_urls_two.py

--- scrapping_urls_two.py
+++ scrapping_urls_two.py
@@ -10,20 +10,32 @@ def validate(url):
         return all([parsed.scheme, parsed.netloc])
     except ValueError:
         return False
+ 
+def extratText(soup):
+    texto_limpo = soup.get_text(separator='\n', strip=True)
+    st.write(texto_limpo)
+ 
+def extractLinks(soup): 
+    for link in soup.find_all('a'):
+        href = link.get('href')
+        if validate(href):  
+            st.write(href, len(href))
 
 async def main(url):
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as resp:
-            st.write(resp.status)
-            htmlText = await resp.text()
-            soup = BeautifulSoup(htmlText , 'html.parser')
-            texto_limpo = soup.get_text(separator='\n', strip=True)
-            st.write(texto_limpo)
-            for link in soup.find_all('a'):
-                href = link.get('href')
-                if validate(href):  
-                    st.write(href, len(href))
+            try:
+                htmlText = await resp.text()
+                soup = BeautifulSoup(htmlText, 'html.parser')   
+                return soup
+            except:
+                return ''
 
-asyncio.run(main('http://www.tjma.jus.br/'))
+soup = asyncio.run(main('http://www.tjma.jus.br/'))
+if len(soup) > 0:
+    extratText(soup)
+    extractLinks(soup)
+
+#https://scrappingurlstwo-aouanptf499cdt98bpmjvg.streamlit.app/
 
  
