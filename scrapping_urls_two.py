c5e3622ba5604be9b5890791cf86575c8cfc14fb
San-Rog <118987989+San-Rog@users.noreply.github.com>
2026-06-30 13:03:52 -0300

    Update scrapping_urls_two.py

--- scrapping_urls_two.py
+++ scrapping_urls_two.py
@@ -3,15 +3,15 @@ import asyncio
 import streamlit as st
 from bs4 import BeautifulSoup
 
-async def main():
+async def main(url):
     async with aiohttp.ClientSession() as session:
-        async with session.get('http://www.tjma.jus.br/') as resp:
+        async with session.get(url) as resp:
             print(resp.status)
             html = await resp.text()
             soup = BeautifulSoup(html, 'html.parser')
             texto_limpo = soup.get_text(separator=' ', strip=True)
             st.write(texto_limpo)
 
-asyncio.run(main())
+asyncio.run(main('http://www.tjma.jus.br/'))
 
  
