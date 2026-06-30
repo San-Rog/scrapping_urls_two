309b9d481b34a120e18aecd1cf583046ee745118
San-Rog <118987989+San-Rog@users.noreply.github.com>
2026-06-30 15:00:11 -0300

    Update scrapping_urls_two.py

--- scrapping_urls_two.py
+++ scrapping_urls_two.py
@@ -12,18 +12,36 @@ def validate(url):
         return False
  
 def extratText(soup, url):
-    with st.spinner('Scrapping do texto da URL {url}...'):
+    with st.spinner(text='Scrapping do texto do site {url}...', show_time=True, width="stretch"):
         texto_limpo = soup.get_text(separator='\n', strip=True)
         st.write(texto_limpo)
  
 def extractLinks(soup, url): 
-    for link in soup.find_all('a'):
-        href = link.get('href')
-        if validate(href):  
-            st.write(href, len(href))
+    with st.spinner(text='Scrapping dos links do site {url}...', show_time=True, width="stretch"):
+        links = soup.find_all('a')
+        for link in links:
+            href = link.get('href')
+            if validate(href):  
+                st.write(href, len(href))
 
 def extracImgs(soup, url):
-    pass
+    with st.spinner(text='Scrapping dos links do site {url}...', show_time=True, width="stretch"):
+        imagens = soup.find_all('img')
+        lista_urls = []
+        for img in imagens:
+            link = img.get('src')
+            if link:
+                link_completo = urljoin(url, link)
+                lista_urls.append(link_completo)
+            lista_urls = list(set(lista_urls))
+            if lista_urls:
+                colunas = st.columns(3) 
+                for i, img_url in enumerate(lista_urls):
+                    col = colunas[i % 3]
+                    with col:
+                        st.image(img_url, caption=f"Imagem {i+1}", use_column_width=True)
+            else:
+                st.info("Nenhuma imagem foi encontrada nesta página.")
 
 async def scrap(url):
     async with aiohttp.ClientSession() as session:
@@ -48,6 +66,9 @@ if __name__ == '__main__':
 
 #https://scrappingurlstwo-aouanptf499cdt98bpmjvg.streamlit.app/
 #https://docs.aiohttp.org/en/stable/client_quickstart.html
+
+
+ 
 
 
  
