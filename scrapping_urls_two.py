1194e0a2c1bf156e1f440bd1bdd273fcb2734d48
San-Rog <118987989+San-Rog@users.noreply.github.com>
2026-07-01 22:04:11 -0300

    Change base URL for web scraping
    Updated the base URL for scraping from TRT8 to TRT15.

--- scrapping_urls_two.py
+++ scrapping_urls_two.py
@@ -161,7 +161,7 @@ class operations():
 class main():
     def __init__(self):
         self.setPage() 
-        urlBase = "https://www.trt8.jus.br/"
+        urlBase = "https://www.trt15.jus.br/"
         objOperation = operations(urlBase, None, None)
         soup = asyncio.run(objOperation.scrap())
         if len(soup) > 0:
