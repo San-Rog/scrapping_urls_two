854b1bb9644fa2c9eeab303a00b45a34082471da
San-Rog <118987989+San-Rog@users.noreply.github.com>
2026-07-01 21:51:50 -0300

    Update scrapping_urls_two.py

--- scrapping_urls_two.py
+++ scrapping_urls_two.py
@@ -161,7 +161,7 @@ class operations():
 class main():
     def __init__(self):
         self.setPage() 
-        urlBase = "view-source:https://transparencia.ma.gov.br/app/v2/pessoal/remuneracao"
+        urlBase = "https://dados.ma.gov.br/?q=search/field_topic/pessoal-49"
         objOperation = operations(urlBase, None, None)
         soup = asyncio.run(objOperation.scrap())
         if len(soup) > 0:
