88388e0ff7bfeef62ad4bbe7d4014cf042286644
San-Rog <118987989+San-Rog@users.noreply.github.com>
2026-07-01 21:31:17 -0300

    Update scrapping_urls_two.py

--- scrapping_urls_two.py
+++ scrapping_urls_two.py
@@ -161,7 +161,7 @@ class operations():
 class main():
     def __init__(self):
         self.setPage() 
-        urlBase = "https://www.trt16.jus.br"
+        urlBase = "https://www.trt16.jus.br/transparencia/prestacao-de-contas/apresentacao"
         objOperation = operations(urlBase, None, None)
         soup = asyncio.run(objOperation.scrap())
         if len(soup) > 0:
